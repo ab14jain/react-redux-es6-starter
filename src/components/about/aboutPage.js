b938f52091f752dc6e7b20db6a28ff216007886d
Abhishek <ab14jain@gmail.com>
2018-04-15 21:58:11 +0530

    updated with new sorce code

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>About</h1>
+                <p1>This application uses react, redux</p1>                
+            </div>
+        )
+    }
+}
+
+export default AboutPage;
